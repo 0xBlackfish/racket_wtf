852c95cfb068906db39bdf889567ec40fd489665
Blackfish <0xBlackfish@gmail.com>
2023-12-21 00:30:17 -0700

    URL string should only show selected campaign ids

--- admin-tools/pages/3_Cancel_Campaigns.py
+++ admin-tools/pages/3_Cancel_Campaigns.py
@@ -117,9 +117,9 @@ def dataframe_with_selections(df):
     )
 
     # Filter the dataframe using the temporary column, then drop the column
-    #selected_rows = edited_df[edited_df.Select]
+    selected_rows = edited_df[edited_df.Select]
     
-    return edited_df.drop('Select', axis=1)
+    return selected_rows.drop('Select', axis=1)
 
 
 selection = dataframe_with_selections(df_display)
