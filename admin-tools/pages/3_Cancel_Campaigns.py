9181703fd173fbacb7254584a63f1476cc7e46bc
Blackfish <0xBlackfish@gmail.com>
2023-12-20 18:45:59 -0700

    Adding selection capability

--- admin-tools/pages/3_Cancel_Campaigns.py
+++ admin-tools/pages/3_Cancel_Campaigns.py
@@ -44,14 +44,67 @@ st.subheader('Current Posts on Campaign Board')
 st.write(' ')
 st.write(' ')
 
-df = pd.DataFrame(
-    {
-        "Animal": ["Lion", "Elephant", "Giraffe", "Monkey", "Zebra"],
-        "Habitat": ["Savanna", "Forest", "Savanna", "Forest", "Savanna"],
-        "Lifespan (years)": [15, 60, 25, 20, 25],
-        "Average weight (kg)": [190, 5000, 800, 10, 350],
-    }
+json_response = response.json()
+
+df = pd.json_normalize(json_response)
+
+df.rename(
+    columns={
+    'id':'campaign_id',
+    'type':'campaign_type',
+    'bidPrice':'bid_price',
+    'desiredEngagements':'desired_engagements',
+    'bidTotal':'bid_total',
+    'bidTotalUnsettled':'bid_total_unsettled',
+    'promotedTweet':'promoted_tweet',
+    'promotedAccountUsername':'promoted_account_username',
+    'promotedTweetAuthorUsername':'promoted_tweet_author_username',
+    'createdBy.username':'created_by_username',
+    }, 
+    inplace=True
 )
+
+df['promoted_account_username'].fillna(df['promoted_tweet_author_username'], inplace=True)
+
+# Put two streamlit filters side by side
+filter1, filter2, filter3 = st.columns(3)
+
+with filter1:
+    promoted_user = st.multiselect('Promoted User', df['promoted_account_username'].sort_values(ascending=True).unique())
+with filter2:
+    created_by_username = st.multiselect('Campaign Creator', df['created_by_username'].sort_values(ascending=True).unique())
+with filter3:
+    campaign_type = st.multiselect('Campaign Type', df['campaign_type'].sort_values(ascending=True).unique())
+
+# Conditionally filter the dataframe based on the user input, but only if the user input is not empty
+if promoted_user and created_by_username and campaign_type:
+    df_filtered = df[df['promoted_account_username'].isin(promoted_user) & df['created_by_username'].isin(created_by_username) & df['campaign_type'].isin(campaign_type)]
+elif promoted_user and created_by_username:
+    df_filtered = df[df['promoted_account_username'].isin(promoted_user) & df['created_by_username'].isin(created_by_username)]
+elif promoted_user and campaign_type:
+    df_filtered = df[df['promoted_account_username'].isin(promoted_user) & df['campaign_type'].isin(campaign_type)]
+elif created_by_username and campaign_type:
+    df_filtered = df[df['created_by_username'].isin(created_by_username) & df['campaign_type'].isin(campaign_type)]
+elif promoted_user:
+    df_filtered = df[df['promoted_account_username'].isin(promoted_user)]
+elif created_by_username:
+    df_filtered = df[df['created_by_username'].isin(created_by_username)]
+elif campaign_type:
+    df_filtered = df[df['campaign_type'].isin(campaign_type)]
+else:
+    df_filtered = df
+
+
+st.write(' ')
+st.write(' ')
+
+df_display = df_filtered[['campaign_id','campaign_type','bid_price','desired_engagements','bid_total','bid_total_unsettled','promoted_tweet','promoted_account_username','created_by_username']]
+
+st.dataframe(df_display,use_container_width=True)
+
+st.write(' ')
+st.write(' ')
+
 
 def dataframe_with_selections(df):
     df_with_selections = df.copy()
@@ -67,9 +120,11 @@ def dataframe_with_selections(df):
 
     # Filter the dataframe using the temporary column, then drop the column
     selected_rows = edited_df[edited_df.Select]
+    
     return selected_rows.drop('Select', axis=1)
 
 
-selection = dataframe_with_selections(df)
+selection = dataframe_with_selections(df_display)
+
 st.write("Your selection:")
 st.write(selection)
