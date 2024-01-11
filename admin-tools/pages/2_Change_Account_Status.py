b63560ef988693512295d3c8f517e459920437d4
Blackfish <0xBlackfish@gmail.com>
2024-01-11 00:55:34 -0700

    Re-baseline

--- admin-tools/pages/2_Change_Account_Status.py
+++ admin-tools/pages/2_Change_Account_Status.py
@@ -45,103 +45,46 @@ st.write(' ')
 st.write(' ')
 
 
-# # Get the current list of Racket users
-# response = requests.get(st.secrets['users_endpoint'])
-# json_response = response.json()
-
-# df = pd.json_normalize(json_response)
-
-# df.rename(
-#     columns={
-#     'id':'racket_user_id',
-#     'createdAt':'created_at',
-#     'username':'twitter_username',
-#     'twitterId':'twitter_id',
-#     'suspend':'is_suspended'
-#     }, 
-#     inplace=True
-# )
-
-
-# # Reorder the columns in the dataframe
-# df = df[['racket_user_id', 'twitter_username', 'twitter_id', 'created_at', 'is_suspended']]
-
-# # Fill all none values with empty strings
-# df.fillna('', inplace=True)
-
-# # Put three streamlit filters side by side
-# filter1, filter2 = st.columns(2)
-
-# with filter1:
-#     twitter_username = st.multiselect('Twitter Username', df['twitter_username'].sort_values(ascending=True).unique())
-# with filter2:
-#     is_suspended = st.selectbox('Is Suspended', df['is_suspended'].sort_values(ascending=True).unique(),index=None)
-
-
-# # Conditionally filter the dataframe based on the user input, but only if the user input is not empty
-# if twitter_username and is_suspended:
-#     df_filtered = df[df['twitter_username'].isin(twitter_username) & df['is_suspended'] == is_suspended]
-# elif twitter_username:
-#     df_filtered = df[df['twitter_username'].isin(twitter_username)]
-# elif is_suspended:
-#     df_filtered = df[df['is_suspended'] == is_suspended]
-# else:
-#     df_filtered = df
-
-
-# st.write(' ')
-# st.write(' ')
-
-# st.dataframe(df_filtered,use_container_width=True)
-# Get the current posts on the campaign board
-response = requests.get(st.secrets['campaigns_endpoint'])
+# Get the current list of Racket users
+response = requests.get(st.secrets['users_endpoint'])
 json_response = response.json()
 
 df = pd.json_normalize(json_response)
 
 df.rename(
     columns={
-    'id':'campaign_id',
-    'type':'campaign_type',
-    'bidPrice':'bid_price',
-    'desiredEngagements':'desired_engagements',
-    'bidTotal':'bid_total',
-    'bidTotalUnsettled':'bid_total_unsettled',
-    'promotedTweet':'promoted_tweet',
-    'promotedAccountUsername':'promoted_account_username',
-    'promotedTweetAuthorUsername':'promoted_tweet_author_username',
-    'createdBy.username':'created_by_username',
+    'id':'racket_user_id',
+    'createdAt':'created_at',
+    'username':'twitter_username',
+    'twitterId':'twitter_id',
+    'suspend':'is_suspended'
     }, 
     inplace=True
 )
 
-df['promoted_account_username'].fillna(df['promoted_tweet_author_username'], inplace=True)
+
+# Reorder the columns in the dataframe
+df = df[['racket_user_id', 'twitter_username', 'twitter_id', 'created_at', 'is_suspended']]
+
+# Fill all none values with empty strings
+df.fillna('', inplace=True)
 
 # Put three streamlit filters side by side
-filter1, filter2, filter3 = st.columns(3)
+filter1, filter2 = st.columns(2)
 
 with filter1:
-    promoted_user = st.multiselect('Promoted User', df['promoted_account_username'].sort_values(ascending=True).unique())
+    twitter_username = st.multiselect('Twitter Username', df['twitter_username'].sort_values(ascending=True).unique())
 with filter2:
-    created_by_username = st.multiselect('Campaign Creator', df['created_by_username'].sort_values(ascending=True).unique())
-with filter3:
-    campaign_type = st.multiselect('Campaign Type', df['campaign_type'].sort_values(ascending=True).unique())
+    is_suspended = st.selectbox('Is Suspended', df['is_suspended'].sort_values(ascending=True).unique(),index=None)
+
 
 # Conditionally filter the dataframe based on the user input, but only if the user input is not empty
-if promoted_user and created_by_username and campaign_type:
-    df_filtered = df[df['promoted_account_username'].isin(promoted_user) & df['created_by_username'].isin(created_by_username) & df['campaign_type'].isin(campaign_type)]
-elif promoted_user and created_by_username:
-    df_filtered = df[df['promoted_account_username'].isin(promoted_user) & df['created_by_username'].isin(created_by_username)]
-elif promoted_user and campaign_type:
-    df_filtered = df[df['promoted_account_username'].isin(promoted_user) & df['campaign_type'].isin(campaign_type)]
-elif created_by_username and campaign_type:
-    df_filtered = df[df['created_by_username'].isin(created_by_username) & df['campaign_type'].isin(campaign_type)]
-elif promoted_user:
-    df_filtered = df[df['promoted_account_username'].isin(promoted_user)]
-elif created_by_username:
-    df_filtered = df[df['created_by_username'].isin(created_by_username)]
-elif campaign_type:
-    df_filtered = df[df['campaign_type'].isin(campaign_type)]
+if twitter_username and is_suspended:
+    df_filtered = df[df['twitter_username'].isin(twitter_username) & df['is_suspended'] == is_suspended]
+elif twitter_username:
+    df_filtered = df[df['twitter_username'].isin(twitter_username)]
+elif is_suspended:
+    df_filtered = df[df['is_suspended'] == is_suspended]
 else:
     df_filtered = df
 
@@ -149,10 +92,7 @@ else:
 st.write(' ')
 st.write(' ')
 
-df_display = df_filtered[['campaign_id','campaign_type','bid_price','desired_engagements','bid_total','bid_total_unsettled','promoted_tweet','promoted_account_username','created_by_username']]
-
-st.dataframe(df_display,use_container_width=True)
-response1 = requests.get(st.secrets['users_endpoint'])
+st.dataframe(df_filtered,use_container_width=True)
 
 st.write(' ')
 st.write(' ')
@@ -168,10 +108,10 @@ def change_account_status(account_change, twitter_usernames):
 
     if account_change == 'Suspend':
         requests.post(st.secrets['suspend_endpoint'] + twitter_usernames_string)
-        # print(st.secrets['suspend_endpoint'] + twitter_usernames_string)
+        print(st.secrets['suspend_endpoint'] + twitter_usernames_string)
     elif account_change == 'Reinstate':
         requests.post(st.secrets['reinstate_endpoint'] + twitter_usernames_string)
-        # print(st.secrets['reinstate_endpoint'] + twitter_usernames_string)
+        print(st.secrets['reinstate_endpoint'] + twitter_usernames_string)
     else:
         st.error('This is an error', icon="🚨")
 
